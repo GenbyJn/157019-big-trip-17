bb1b8d077b2374ae5ce3f88184ee36d4b8fc7815
Vadim <rootfig@gmail.com>
2022-05-03 11:12:31 +0300

    fix

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -3,15 +3,18 @@ import { render, RenderPosition } from '../render.js';
 import MainInfoView from '../view/main-info-view.js';
 import MainFiltersView from '../view/main-filters-view.js';
 import SortItemsView from '../view/sort-view.js';
-
 import EditorPointView from '../view/edit-point-view.js';
+import EventTripView from '../view/event-trip-view.js';
 
 export default class Presentor {
   init = (containers) => {
     const {headerTripMain, headerTripMainFilters, mainTripEvents, tripEventsList} = containers;
     render(new MainInfoView(), headerTripMain, RenderPosition.AFTERBEGIN);
     render(new MainFiltersView(), headerTripMainFilters);
-    render(new SortItemsView(), mainTripEvents);
+    render(new SortItemsView(), mainTripEvents, RenderPosition.AFTERBEGIN);
     render(new EditorPointView(), tripEventsList);
+    for (let i = 0; i < 3; i++) {
+      render(new EventTripView(), tripEventsList);
+    }
   };
 }
