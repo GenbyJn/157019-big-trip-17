2300e90cc65cf3e10f2c66ff39976d60f04f59a5
Vadim <rootfig@gmail.com>
2022-06-02 06:11:30 +0300

    Задача 3-1

--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -1,4 +1,4 @@
-import { render, RenderPosition } from '@/render';
+import { render } from '@/render';
 
 import PointListView from '@view/point-list-view.js';
 import PointListItemView from '../view/point-list-item-view';
@@ -6,7 +6,6 @@ import EditPointView from '@view/edit-point/edit-point-view.js';
 
 export default class PointListPresenter {
   #pointListView = new PointListView();
-  #pointListItemView = null;
   #editPointView = null;
   #pointModel = null;
   #points = [];
@@ -19,11 +18,8 @@ export default class PointListPresenter {
 
     this.#editPointView = new EditPointView(this.#points[0]);
 
-    render(this.#editPointView, this.#pointListView.element, RenderPosition.AFTERBEGIN);
-
     for (let i = 0; i < this.#points.length; i++) {
       this.#renderEditPoint(this.#points[i]);
-      // render(new PointListItemView(point), this.#pointListView.element);
     }
 
     render(this.#pointListView, mainTripEventsElement);
@@ -31,9 +27,42 @@ export default class PointListPresenter {
   }
 
   #renderEditPoint = (point) => {
-    const editPointComponent = new PointListItemView(point);
+    const pointItemView = new PointListItemView(point);
+    const editPointView = new EditPointView(point);
 
-    render(editPointComponent, this.#pointListView.element);
+    const replacePointToEditPoint = () => {
+      this.#pointListView.element.replaceChild(editPointView.element, pointItemView.element);
+    };
+
+    const replaceEditPointToPoint = () => {
+      this.#pointListView.element.replaceChild(pointItemView.element, editPointView.element);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditPointToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    pointItemView.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replacePointToEditPoint();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    editPointView.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceEditPointToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    editPointView.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceEditPointToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(pointItemView, this.#pointListView.element);
   };
 
 }
